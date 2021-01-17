f29a478eb3888504a63910dfe89d150c9a7edb7f
MALEKYLA <58940885+MALEKYLA@users.noreply.github.com>
2021-01-17 20:17:37 +0200

    Update TechnicalWriter.java

--- src/domain/TechnicalWriter.java
+++ src/domain/TechnicalWriter.java
@@ -1,3 +1,15 @@
+  
 package domain;
+
+/**
+ * Клас {@code TechnicalWriter} представляє об'єкт технічного писателя.
+ * 
+ * @see  Artist
+ * @see  Manager
+ * @see  GraphicIllustrator
+ * @see  Employee
+ * @see  Editor
+ */
 public class TechnicalWriter extends Artist {
+	
 }
