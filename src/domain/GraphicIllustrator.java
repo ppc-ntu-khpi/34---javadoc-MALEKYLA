651e0572c74a62a405b69dfc65872e3fed221e4f
MALEKYLA <58940885+MALEKYLA@users.noreply.github.com>
2021-01-17 20:16:44 +0200

    Update GraphicIllustrator.java

--- src/domain/GraphicIllustrator.java
+++ src/domain/GraphicIllustrator.java
@@ -1,3 +1,15 @@
+  
 package domain;
+
+/**
+ * Клас {@code GraphicIllustrator} представляє об'єкт графічного ілюстратора.
+ * 
+ * @see  Artist
+ * @see  TechnicalWriter
+ * @see  Manager
+ * @see  Employee
+ * @see  Editor
+ */
 public class GraphicIllustrator extends Artist {
+	
 }
